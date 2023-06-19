f6dccbb2697f120ab6d72c14575ba83759a4b4ca
rawdakhallaf <rawdamkm,rm@gmail.com>
2023-06-19 16:10:19 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,13 @@
  * Return: Always 0.
  */
 int main(void)
-{ 
+{
 	char letter;
-	 for(letter "a"; letter <= 'z'; letter++)
-		 putchar(letter);
 
-	 putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
 
 	return (0);
-
-
-
 }
