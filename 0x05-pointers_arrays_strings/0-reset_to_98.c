89d2659c65059ebb27867462082916534de64928
rawdakhallaf <rawdamkm,rm@gmail.com>
2023-06-27 17:09:56 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,14 +5,9 @@
  * main - check the code 
  *
  * Return: Always 0.
+ *
  */
-int main(void)
+void reset_to_98(int *n)
 {
-    int  n;
-   
-    n = 98;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	*n = 98;
 }
